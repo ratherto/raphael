d994a99b13a2b74c12aec38dcd1e967c706dca39
junghwan-park <junghwan.park@nhnent.com>
2017-02-01 18:13:20 +0900

    Deploy v2.2.6b

--- raphael.js
+++ raphael.js
@@ -6531,7 +6531,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        $(node, {x: a.x, y: a.y});
 	        el._.dirty = 1;
 	        var bb = el._getBBox(),
-	            dif = a.y - (bb.y + bb.height / 2);
+	            dif = a.y - (bb.y + bb.height * 8 / 10);
 	        if (bb.height) dif && R.is(dif, "finite") && $(tspans[0], {dy: dif});
 	    },
 	    getRealNode = function (node) {
